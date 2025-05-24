87c8500187698a279ff210303052b2bbe5b00def
asadalilodhi <asadalilodhi09@gmail.com>
2025-05-24 16:14:31 +0500

    implemented commented suggestions and added GoConvey

--- Hello_World/hello_world_test.go
+++ Hello_World/hello_world_test.go
@@ -1,36 +1,23 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
 
 func TestHello(t *testing.T) {
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := hello("Chris","")
-		want := "Hello, Chris"
-		assertCorrectMessage(t, got, want)
+	Convey("Given a name and no language", t, func() {
+		result := hello("Chris", "")
+		Convey("It should return 'Hello, Chris'", func() {
+			So(result, ShouldEqual, "Hello, Chris")
+		})
 	})
 
-	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := hello("","")
-		want := "Hello, World"
-		assertCorrectMessage(t, got, want)
+	Convey("Given an empty name", t, func() {
+		result := hello("", "")
+		Convey("It should default to 'Hello, World'", func() {
+			So(result, ShouldEqual, "Hello, World")
+		})
 	})
-
-	t.Run("in Spanish", func(t *testing.T) {
-		got := hello("Elodie", "Spanish")
-		want := "Hola, Elodie"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("in French", func(t *testing.T) {
-		got := hello("Elodie", "French")
-		want := "Bonjour, Elodie"
-		assertCorrectMessage(t, got, want)
-	})
-}
-
-func assertCorrectMessage(t testing.TB, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
 }
